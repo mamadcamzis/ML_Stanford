be047385bd0c99ecfaddc0dd9a7b6d057f2f9a63
Mamadou CAMARA <mamadcamzis@MacBook-Air-de-Mamadou.local>
2018-07-10 00:21:04 +0200

    right logistic cost

--- week-3/costFunction.m
+++ week-3/costFunction.m
@@ -12,7 +12,7 @@ J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
+% I%nstructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
@@ -21,12 +21,9 @@ grad = zeros(size(theta));
 %
 
 h_theta = sigmoid(X*theta);
-for  i=1:m,
-    J = J + 1/m * ( -y(i)*log(h_theta) - (1-y(i))*log(1-h_theta) );
-end
+J = (1/m )* sum(-y' * log(h_theta) - (1-y)' * log(1-h_theta));
 
 grad = 1/m * (X'* (h_theta-y));
-
 
 
 % =============================================================
