533166b766492acc47782f40345a27464d7d8087
Mamadou Camara <camara0388@gmail.com>
2018-07-29 19:13:10 +0200

    select  best threshold and F1 score

--- week-9/selectThreshold.m
+++ week-9/selectThreshold.m
@@ -26,16 +26,16 @@ for epsilon = min(pval):stepsize:max(pval)
 
 
 
+    predictions = (pval < epsilon);
+    tp = sum((predictions == 1) & yval == 1);
+    fp = sum((predictions == 1) & yval == 0);
+    fn = sum((predictions == 0) & yval == 1);
+    precision = tp/(tp + fp);
+    recall = tp/(tp+fn);
+    F1 = 2*precision*recall/(precision + recall);
 
 
-
-
-
-
-
-
-
-    % =============================================================
+   % =============================================================
 
     if F1 > bestF1
        bestF1 = F1;
